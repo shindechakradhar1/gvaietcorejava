78f0657d6ade19979ead82cc7dc920afa987139a
shindechakradhar1 <154117953+shindechakradhar1@users.noreply.github.com>
2025-04-10 19:01:13 +0530

    Delete generic directory

--- generic/src/com/gvacharya/generic/mobilebill/framework/MobileBill.java
+++
@@ -1,6 +0,0 @@
-package com.gvacharya.generic.mobilebill.framework;
-
-@FunctionalInterface
-public interface MobileBill<T extends Payable> {
-	public Float generateBill(T noOfDays);
-}
--- generic/src/com/gvacharya/generic/mobilebill/framework/Payable.java
+++
@@ -1,6 +0,0 @@
-package com.gvacharya.generic.mobilebill.framework;
-
-// marker interface
-public interface Payable {
-
-}
