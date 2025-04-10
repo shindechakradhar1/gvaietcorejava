f67738c16675ea14fb64cc1e20c17b56bb616dc0
shindechakradhar1 <154117953+shindechakradhar1@users.noreply.github.com>
2025-04-10 19:09:44 +0530

    Delete casestudy1 directory

--- casestudy1/src/casestudy1/bank/framework/BankFactory.java
+++
@@ -1,7 +0,0 @@
-package casestudy1.bank.framework;
-
-public interface BankFactory {
-
-	SavingAcc getSavingAcc(int accNo,String accNm,float accBal,boolean isSalaried);
-	CurrentAcc getCurrentAcc(int accNo,String accNm,float accBal,float creditLimit);
-}
