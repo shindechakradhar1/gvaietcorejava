e81654d82276c9fb0de2803257ab76673e6ed317
shindechakradhar1 <154117953+shindechakradhar1@users.noreply.github.com>
2025-04-10 18:59:03 +0530

    Delete interfaces directory

--- interfaces/src/com/gvacharya/interfaces/demo/Addition.java
+++
@@ -1,8 +0,0 @@
-package com.gvacharya.interfaces.demo;
-
-public class Addition implements ArithematicOperation{
-
-	public int operation(int number1, int number2) {
-		return number1+number2;
-	}
-}
